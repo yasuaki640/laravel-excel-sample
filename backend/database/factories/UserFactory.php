2035942986b119db8d338b781ce64b82e3669213
yasuaki640 <yasuaki640@gmail.com>
2021-07-25 22:34:19 +0900

    add columns to factory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -24,6 +24,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'd_o_b' => $this->faker->date(),
+            'sex' => $this->faker->numberBetween(0, 1),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
