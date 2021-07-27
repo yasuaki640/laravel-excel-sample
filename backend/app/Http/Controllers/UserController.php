88c3bfbb9e6ffaeb4a7feb056508ba3531e4d956
yasuaki640 <yasuaki640@gmail.com>
2021-07-27 22:06:52 +0900

    update comment

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Contracts\View\View;
 
+/**
+ * Class UserController
+ * @package App\Http\Controllers
+ */
 class UserController extends Controller
 {
+    /**
+     * Show download excel files form
+     *
+     * @return View
+     */
     public function showDownloadForm(): View
     {
         return view('excel.index');
