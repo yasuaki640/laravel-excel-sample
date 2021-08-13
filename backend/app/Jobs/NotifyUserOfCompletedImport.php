91dcfeb9064a31f60dc264389846ec0d781bd4e1
yasuaki640 <yasuaki640@gmail.com>
2021-08-13 22:58:51 +0900

    途中で面ご

---
+++ backend/app/Jobs/NotifyUserOfCompletedImport.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class NotifyUserOfCompletedImport implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var User
+     */
+    private User $user;
+
+    /**
+     * @var string
+     */
+    private string $fileName;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param User $user
+     * @param string $fileName
+     */
+    public function __construct(User $user, string $fileName)
+    {
+        $this->user = $user;
+        $this->fileName = $fileName;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
