b0ceaf4828601fe51a67b97868e3a294623fc14c
yasuaki640 <yasuaki640@gmail.com>
2021-08-20 15:23:57 +0900

    add form of queue import

--- backend/resources/views/excel/import.blade.php
+++ backend/resources/views/excel/import.blade.php
@@ -4,6 +4,7 @@
     <section>
         <h1>Laravel/Excel Sample app</h1>
         <div>
+            <p>Import</p>
             <form
                 action="{{ route('users.excel.import.upload') }}"
                 method="POST"
@@ -15,6 +16,22 @@
                 </div>
                 <div>
                     <input type="submit" value="Import">
+                </div>
+            </form>
+        </div>
+        <div>
+            <p>Import by queue</p>
+            <form
+                action="{{ route('users.excel.import.queue') }}"
+                method="POST"
+                enctype="multipart/form-data"
+            >
+                @csrf
+                <div>
+                    <input type="file" id="users" name="users">
+                </div>
+                <div>
+                    <input type="submit" value="Import by queue">
                 </div>
             </form>
         </div>
