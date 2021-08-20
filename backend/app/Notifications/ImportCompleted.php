02aed71a8efb03ef98913883d347d5b77e381ea6
yasuaki640 <yasuaki640@gmail.com>
2021-08-20 21:49:11 +0900

    fghui

--- backend/app/Notifications/ImportCompleted.php
+++ backend/app/Notifications/ImportCompleted.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Notifications;
 
-use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
@@ -33,7 +32,7 @@ class ImportCompleted extends Notification
      * @param mixed $notifiable
      * @return array
      */
-    public function via($notifiable)
+    public function via($notifiable): array
     {
         return ['mail'];
     }
@@ -44,12 +43,11 @@ class ImportCompleted extends Notification
      * @param mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail($notifiable)
+    public function toMail($notifiable): MailMessage
     {
         return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+            ->line('Import has been completed.')
+            ->action('Return to the app', route('users.excel.import.import-form'));
     }
 
     /**
@@ -58,7 +56,7 @@ class ImportCompleted extends Notification
      * @param mixed $notifiable
      * @return array
      */
-    public function toArray($notifiable)
+    public function toArray($notifiable): array
     {
         return [
             //
